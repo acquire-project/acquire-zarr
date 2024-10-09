2b648c176f63682d59118ad82aa3f932b466e673
Alan Liddell <aliddell@chanzuckerberg.com>
2024-10-09 11:15:17 -0400

    python -m build working (on Windows anyway)

--- setup.py
+++ setup.py
@@ -14,8 +14,6 @@ class CMakeExtension(Extension):
 class CMakeBuild(build_ext):
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-
-        # Required for auto-detection of auxiliary "native" libs
         if not extdir.endswith(os.path.sep):
             extdir += os.path.sep
 
@@ -23,49 +21,51 @@ class CMakeBuild(build_ext):
 
         cfg = "Debug" if self.debug else "Release"
 
-        # Set CMAKE_BUILD_TYPE
         cmake_args = [
             "--preset=default",
             f"-DCMAKE_BUILD_TYPE={cfg}",
             "-DBUILD_PYTHON=ON",
             "-DBUILD_TESTS=OFF"
         ]
-        # cmake_args += [f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}"]
 
         if self.compiler.compiler_type == "msvc":
             cmake_args.append("-DVCPKG_TARGET_TRIPLET=x64-windows-static")
 
         build_args = ['--config', cfg]
 
-        # Set CMAKE_BUILD_PARALLEL_LEVEL for parallel builds
         if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
             if hasattr(self, "parallel") and self.parallel:
                 build_args += [f"-j{self.parallel}"]
 
-        # Build the entire project
+        os.makedirs(build_dir, exist_ok=True)
         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=build_dir)
         subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=build_dir)
 
-        # Move the built extension to the correct location
         built_ext = os.path.join(build_dir, 'python', cfg, 'acquire_zarr*.pyd')
-        self.move_file(built_ext, self.get_ext_fullpath(ext.name))
+        matching_files = glob.glob(built_ext)
+        if not matching_files:
+            raise RuntimeError(f"Could not find any files matching {built_ext}")
 
-    def move_file(self, src, dst):
+        # Move the built extension to the correct location
+        dst = self.get_ext_fullpath(ext.name)
+        self.move_file(matching_files, dst)
+
+    def move_file(self, src_files, dst):
         import shutil
         try:
             os.makedirs(os.path.dirname(dst), exist_ok=True)
-            for filename in glob.glob(src):
-                shutil.move(filename, dst)
+            for filename in src_files:
+                shutil.copy2(filename, dst)
         except Exception as e:
-            print(f"Error moving file from {src} to {dst}: {e}")
+            print(f"Error moving files to {dst}: {e}")
             raise
 
 
 setup(
     name='acquire-zarr',
     version='0.0.1',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Alan Liddell',
+    author_email='aliddell@chanzuckerberg.com',
     description='Python bindings for acquire-zarr',
     long_description='',
     ext_modules=[CMakeExtension('acquire_zarr')],
