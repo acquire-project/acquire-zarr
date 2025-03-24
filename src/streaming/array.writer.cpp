ce5212105413e5e02ef688d89d2d04ab4ad3e10f
Alan Liddell <aliddell@chanzuckerberg.com>
2025-03-24 12:47:54 -0400

    (wip) limit number of omp threads

--- src/streaming/array.writer.cpp
+++ src/streaming/array.writer.cpp
@@ -9,10 +9,9 @@
 #include <functional>
 #include <stdexcept>
 
-#include <omp.h>
-
 #ifdef min
 #undef min
+#undef max
 #endif
 
 bool
@@ -231,7 +230,8 @@ zarr::ArrayWriter::write_frame_to_chunks_(std::span<const std::byte> data)
     size_t bytes_written = 0;
     const auto n_tiles = n_tiles_x * n_tiles_y;
 
-#pragma omp parallel for reduction(+ : bytes_written)
+#pragma omp parallel for reduction(+ : bytes_written)                          \
+  num_threads(std::max(thread_pool_->n_threads() / 2, 1u))
     for (auto tile = 0; tile < n_tiles; ++tile) {
         const auto i = tile / n_tiles_x;
         const auto j = tile % n_tiles_x;
